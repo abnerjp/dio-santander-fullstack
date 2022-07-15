2836541d1c42f499cb23154142095766f7353741
Abner J Pelisser <abner.pelisser@gmail.com>
2022-07-15 18:27:42 -0300

    atividade - modulos js

---
+++ 13-modulos/main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade("Teste", 30));
+console.log(mostraCidade("Fulano", "Presidente Prudente"));
+console.log(mostraHobby("Fulano", "Programar"));
