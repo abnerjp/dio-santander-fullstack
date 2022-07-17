512b3991ef242bf7a4c595d80aeef689ade166b2
Abner J Pelisser <abner.pelisser@gmail.com>
2022-07-16 22:32:36 -0300

    criando propriedades readonly e private

--- 15-typescript/index.ts
+++ 15-typescript/index.ts
@@ -1,14 +1,25 @@
-interface IUsuario {
-  id: string;
-  email: string,
-  cargo?: "gerente" | "coordenador" | "supervisor" | "funcionario";
+interface ICachorro {
+  readonly nome: string;
+  readonly idade: number;
+  readonly parqueFavorito?: string
 }
 
+type CachorroSomenteLeitura = {
+  +readonly [K in keyof ICachorro]-?: ICachorro[K];
+}
 
-function redirecione (usuario: IUsuario) {
-  if (usuario.cargo) {
-    // redirecionar(usuario.cargo);
+class MeuCachorro implements CachorroSomenteLeitura {
+  nome;
+  idade;
+  parqueFavorito;
+
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
   }
-
-  // redirecionar para a área do usuário
 }
+
+const cao = new MeuCachorro("Apolo", 14);
+cao.idade = 8;
+
+console.log(cao)
