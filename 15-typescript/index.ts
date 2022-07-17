e8e898ce4b5216d56026d98778dcb30b723e05e4
Abner J Pelisser <abner.pelisser@gmail.com>
2022-07-16 21:54:11 -0300

    interfaces ts

--- 15-typescript/index.ts
+++ 15-typescript/index.ts
@@ -1,4 +1,25 @@
-function soma (a: number, b: number) {
-  return a + b;
+// types
+// interfaces
+
+interface IAnimal {
+  nome: string;
+  tipo: "terrestre" | "aquático";
+  executarSom(alturaEmDecibeis: number): void;
 }
 
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean;
+}
+
+const animal : IAnimal = {
+  nome: "Elefante",
+  tipo: "terrestre",
+  executarSom: (alturaEmDecibeis) => console.log(`${alturaEmDecibeis}dB`)
+}
+
+const felino: IFelino = {
+  nome: "Leão",
+  tipo: "terrestre",
+  visaoNoturna: true,
+  executarSom: (alturaEmDecibeis) => console.log(`${alturaEmDecibeis}dB`)
+}
