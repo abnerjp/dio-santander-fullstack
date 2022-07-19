293cdba027449a19c4adede804efbe01754b927d
Abner J Pelisser <abner.pelisser@gmail.com>
2022-07-19 09:08:14 -0300

    relacao entre tipos de variaveis - ts

--- 16-typescript02/exemplo01/app.js
+++ 16-typescript02/exemplo01/app.js
@@ -2,9 +2,15 @@
 let button = document.getElementById("button");
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
-function adicionarNumero(numero1, numero2) {
-    return numero1 + numero2;
+function adicionarNumero(numero1, numero2, devePrintar, frase) {
+    let resultado = numero1 + numero2;
+    if (devePrintar) {
+        console.log(frase + resultado);
+    }
+    return resultado;
 }
+let frase = "O valor da soma é: ";
+let devePrintar = true;
 button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
-    console.log(adicionarNumero(Number(input1 === null || input1 === void 0 ? void 0 : input1.value), Number(input2 === null || input2 === void 0 ? void 0 : input2.value)));
+    console.log(adicionarNumero(Number(input1 === null || input1 === void 0 ? void 0 : input1.value), Number(input2 === null || input2 === void 0 ? void 0 : input2.value), devePrintar, frase));
 });
--- 16-typescript02/exemplo01/app.ts
+++ 16-typescript02/exemplo01/app.ts
@@ -2,10 +2,18 @@ let button = document.getElementById("button");
 let input1 = document.getElementById("input1") as HTMLInputElement;
 let input2 = document.getElementById("input2") as HTMLInputElement;
 
-function adicionarNumero(numero1: number, numero2: number) {
-  return numero1 + numero2;
+function adicionarNumero(numero1: number, numero2: number, devePrintar: boolean, frase: string) {
+  let resultado = numero1 + numero2;
+  if (devePrintar) {
+    console.log(frase + resultado)
+  }
+  
+  return resultado;
 }
 
+let frase = "O valor da soma é: ";
+let devePrintar = true;
+
 button?.addEventListener("click", () => {
-  console.log(adicionarNumero(Number(input1?.value), Number(input2?.value)));
+  console.log(adicionarNumero(Number(input1?.value), Number(input2?.value), devePrintar, frase));
 });
