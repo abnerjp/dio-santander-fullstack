9b307f2c301c17fb64f3deb80c440998be5887a8
Abner J Pelisser <abner.pelisser@gmail.com>
2022-07-19 21:57:59 -0300

    tipo de retorno void na funcao

--- 16-typescript02/exemplo01/app.js
+++ 16-typescript02/exemplo01/app.js
@@ -1,5 +1,5 @@
 "use strict";
-function somarValoresNumericos(numero1, numero2) {
-    return numero1 + numero2;
+function somarValoresNumericos1(numero1, numero2) {
+    console.log(numero1 + numero2);
 }
 console.log(somarValoresNumericos(1, 2));
--- 16-typescript02/exemplo01/app.ts
+++ 16-typescript02/exemplo01/app.ts
@@ -1,5 +1,5 @@
-function somarValoresNumericos(numero1: number, numero2: number): number {
-  return numero1 + numero2;
+function somarValoresNumericos1(numero1: number, numero2: number): void {
+  console.log(numero1 + numero2);
 }
 
 console.log(somarValoresNumericos(1, 2));
